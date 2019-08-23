f545270c33ff1ea7f3900096c39f9965d23ab614
hereisfahad <fahadjabbar31@gmail.com>
2019-08-23 22:33:48 +0500

    removing yarn.lock

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -13,28 +13,36 @@ class Directory extends React.Component {
         {
           title: 'hats',
           imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+          size: '',
+          linkUrl:'hats',
           id: 1
         },
         {
           title: 'jackets',
           imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+          size: '',
+          linkUrl:'',
           id: 2
         },
         {
           title: 'sneakers',
           imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+          size: '',
+          linkUrl:'',
           id: 3
         },
         {
           title: 'womens',
           imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
           size: 'large',
+          linkUrl:'',
           id: 4
         },
         {
           title: 'mens',
           imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
           size: 'large',
+          linkUrl:'',
           id: 5
         }
       ]
@@ -44,8 +52,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className='directory-menu'>
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ title, imageUrl, id, size, linkUrl }) => (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} linkUrl={linkUrl} />
         ))}
       </div>
     );
