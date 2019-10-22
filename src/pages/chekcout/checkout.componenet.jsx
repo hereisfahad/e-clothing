2e38a450a8f9a8fda9f825d171307e3f81b31e14
hereisfahad <fahadjabbar31@gmail.com>
2019-10-23 01:31:09 +0500

    removing stripe

--- src/pages/chekcout/checkout.componenet.jsx
+++ src/pages/chekcout/checkout.componenet.jsx
@@ -1,53 +1,53 @@
-import React from 'react';
+import React from "react";
 
-import { connect } from 'react-redux';
-import { selectCartItems, selectCartTotal} from '../../redux/cart/cart-selectors';
+import { connect } from "react-redux";
+import {
+  selectCartItems,
+  selectCartTotal
+} from "../../redux/cart/cart-selectors";
 
-import CheckoutItem from '../../components/checkoutItem/checkout-item.component';
-import StripeCheckoutButton from '../../components/stripebutton/stripe-button.component';
+import CheckoutItem from "../../components/checkoutItem/checkout-item.component";
+// import StripeCheckoutButton from '../../components/stripebutton/stripe-button.component';
 
-import './checkout.styles.scss';
+import "./checkout.styles.scss";
 
-const Checkout = ({ cartItems, total }) =>(
-    <div className="checkout-page">
-        <div className="checkout-header">
-            <div className="header-block">
-                <span>Product</span>
-            </div>
-            <div className="header-block">
-                <span>Description</span>
-            </div>
-            <div className="header-block">
-                <span>Quantity</span>
-            </div>
-            <div className="header-block">
-                <span>Prive</span>
-            </div>
-            <div className="header-block">
-                <span>Remove</span>
-            </div>
-        </div>
-        {cartItems.map(cartItem => (
-                <CheckoutItem key={cartItem.id} cartItem={cartItem} />
-            ))
-        }
-        <div className="total"> 
-            <span>Total: ${total}</span>
-        </div>
-        <div className='test-warning'>
-            *Please use the following test credit card for payments*
-            <br />
-            4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
-        </div>
-        <StripeCheckoutButton price={total} />
+const Checkout = ({ cartItems, total }) => (
+  <div className="checkout-page">
+    <div className="checkout-header">
+      <div className="header-block">
+        <span>Product</span>
+      </div>
+      <div className="header-block">
+        <span>Description</span>
+      </div>
+      <div className="header-block">
+        <span>Quantity</span>
+      </div>
+      <div className="header-block">
+        <span>Prive</span>
+      </div>
+      <div className="header-block">
+        <span>Remove</span>
+      </div>
     </div>
+    {cartItems.map(cartItem => (
+      <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+    ))}
+    <div className="total">
+      <span>Total: ${total}</span>
+    </div>
+    {/* <div className="test-warning">
+      *Please use the following test credit card for payments*
+      <br />
+      4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+    </div> */}
+    {/* <StripeCheckoutButton price={total} /> */}
+  </div>
 );
 
-const mapStateToProps = (state) =>(
-    {
-        cartItems: selectCartItems(state),
-        total: selectCartTotal(state)
-    }
-);
+const mapStateToProps = state => ({
+  cartItems: selectCartItems(state),
+  total: selectCartTotal(state)
+});
 
 export default connect(mapStateToProps)(Checkout);
