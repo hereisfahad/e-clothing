20d5c6d8d8b199538a087d9f9258a524ae38b82a
hereisfahad <fahadjabbar31@gmail.com>
2019-11-13 14:16:11 +0500

    added contact page

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,55 +1,66 @@
 //header component needs currentUser for navbar
-import React from 'react';
-import { Link, BrowserRouter, Route, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React from "react";
+import { Link, BrowserRouter, Route, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
 
-import HomePage from '../../pages/homepage/homepage.component';
-import ShopPage from '../../pages/shopPage/shopPage.component';
-import SignInAndSignUpPage from '../../pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import CartIcon from '../../components/cartIcon/cart-icon.component';
-import CardDropdown from '../../components/cartDropdown/cart-dropdown.component';
-import Checkout from '../../pages/chekcout/checkout.componenet';
-import {auth} from '../../firebase/firebase.utils';//use brakets :)
+import HomePage from "../../pages/homepage/homepage.component";
+import ShopPage from "../../pages/shopPage/shopPage.component";
+import Contact from "../../pages/contact/contact.component";
+import SignInAndSignUpPage from "../../pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import CartIcon from "../../components/cartIcon/cart-icon.component";
+import CardDropdown from "../../components/cartDropdown/cart-dropdown.component";
+import Checkout from "../../pages/chekcout/checkout.componenet";
+import { auth } from "../../firebase/firebase.utils"; //use brakets :)
 
-import { selectCurrentUser } from '../../redux/user/user-selectors';
-import { selectHiddenStatus } from '../../redux/cart/cart-selectors';
+import { selectCurrentUser } from "../../redux/user/user-selectors";
+import { selectHiddenStatus } from "../../redux/cart/cart-selectors";
 
-import {ReactComponent as Logo} from './crown.svg'
-import './header.styles.scss';
+import { ReactComponent as Logo } from "./crown.svg";
+import "./header.styles.scss";
 
-const Header = ({currentUser, hidden}) => (
-    <BrowserRouter>
-        <div className="header">
-            <Link className="logo-container" to="/">
-                <Logo/>
-            </Link>
-            <div className="options">
-                <Link className="option" to="/shop">Shop</Link>
-                <Link className="option" to="/contact">Contact</Link>
-                {
-                    currentUser ?
-                    <Link className="option" to="/SignIn/" onClick={() => auth.signOut()}>SignOut</Link>
-                    :
-                    (<Link className="option" to="/SignIn">SignIn</Link>)
-                }
-                <CartIcon />
-            </div>
-            {
-                hidden? null:<CardDropdown/>
-            }
-            
-        </div>
-        <Route path="/" exact component={HomePage} />
-        <Route path="/shop/" component={ShopPage} />
-        <Route path="/contact/" component={ShopPage} />
-        <Route exact path="/checkout" component={Checkout} />
-        <Route exact path="/SignIn/"  render={()=> currentUser ? ( <Redirect to='./'/>) :<SignInAndSignUpPage/>} />
-    </BrowserRouter>
+const Header = ({ currentUser, hidden }) => (
+  <BrowserRouter>
+    <div className="header">
+      <Link className="logo-container" to="/">
+        <Logo />
+      </Link>
+      <div className="options">
+        <Link className="option" to="/shop">
+          Shop
+        </Link>
+        <Link className="option" to="/contact">
+          Contact
+        </Link>
+        {currentUser ? (
+          <Link className="option" to="/SignIn/" onClick={() => auth.signOut()}>
+            SignOut
+          </Link>
+        ) : (
+          <Link className="option" to="/SignIn">
+            SignIn
+          </Link>
+        )}
+        <CartIcon />
+      </div>
+      {hidden ? null : <CardDropdown />}
+    </div>
+    <Route path="/" exact component={HomePage} />
+    <Route path="/shop/" component={ShopPage} />
+    <Route path="/contact/" component={Contact} />
+    <Route exact path="/checkout" component={Checkout} />
+    <Route
+      exact
+      path="/SignIn/"
+      render={() =>
+        currentUser ? <Redirect to="./" /> : <SignInAndSignUpPage />
+      }
+    />
+  </BrowserRouter>
 );
 
-const mapStateToProps = (state) => ({
-    currentUser: selectCurrentUser(state),
-    hidden: selectHiddenStatus(state)
+const mapStateToProps = state => ({
+  currentUser: selectCurrentUser(state),
+  hidden: selectHiddenStatus(state)
 });
 export default connect(mapStateToProps)(Header);
 
---
+++ src/pages/contact/contact.component.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import "./contact.styles.scss";
+
+function Contact() {
+  const iconStyles = {
+    marginRight: "10px",
+    color: "black",
+    fontSize: "30px"
+  };
+  const h2Styles = {
+    color: "#9a9a3ec9",
+    paddingTop: "0px",
+    marginBottom: "1.5rem",
+    fontSize: "2rem"
+  };
+  return (
+    <div
+      className="showCaseDiv animated lightSpeedIn textDiv"
+      style={{ textAlign: "center", margin: "100px auto" }}
+    >
+      <h2 style={h2Styles}>Don't be a stranger</h2>
+      <p style={{ color: "#3e3e36ed", marginBottom: "26px" }}>
+        Feel free to get in touch with me. I am always open to discussing new
+        projects, createive ideas or opportunity to be a part of your visions.
+      </p>
+      <p
+        style={{
+          borderBottom: "1px solid black",
+          color: "#BBBBBD",
+          marginBottom: "15"
+        }}
+      >
+        You can reach me on these platforms.
+      </p>
+      <a
+        href="https://github.com/hereisfahad"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        <i className="fab fa-github grow" style={iconStyles}></i>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/mrfahadjabbar/"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        <i className="fab fa-linkedin grow" style={iconStyles}></i>
+      </a>
+      <a
+        href="https://www.twitter.com/hereisfahad"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        <i className="fab fa-twitter grow" style={iconStyles}></i>
+      </a>
+      <a
+        href="https://www.facebook.com/hereisfahad"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        <i className="fab fa-facebook grow" style={iconStyles}></i>
+      </a>
+    </div>
+  );
+}
+
+export default Contact;
